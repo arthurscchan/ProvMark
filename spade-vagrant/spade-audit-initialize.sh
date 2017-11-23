3b7c756c52c4f375451b1fe0dd74d55cfb32e295
Ashish Gehani <ashish.gehani@sri.com>
2017-11-23 09:36:38 -0800

    Removed duplicate JDK install

--- spade-vagrant/spade-audit-initialize.sh
+++ spade-vagrant/spade-audit-initialize.sh
@@ -4,7 +4,6 @@ sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
-sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
 
 sudo apt-get install -y git libaudit-dev auditd libfuse-dev fuse pkg-config lsof uthash-dev
@@ -22,4 +21,3 @@ sudo service auditd restart
 ## ProvMark stuff
 # 
 git clone https://github.com/arthurscchan/ProvMark.git 
-
