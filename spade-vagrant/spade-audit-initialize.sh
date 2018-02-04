473e9cbd360625775c96b2e56b19482c447ee174
James Cheney <jcheney@inf.ed.ac.uk>
2018-02-04 18:27:40 +0000

    fix bug in Clingo template, also change network address to avoid conflict

--- spade-vagrant/spade-audit-initialize.sh
+++ spade-vagrant/spade-audit-initialize.sh
@@ -17,7 +17,10 @@ sudo chmod ug+s lib/spadeAuditBridge
 sudo sed -i "s/active = no/active = yes/" /etc/audisp/plugins.d/af_unix.conf
 sudo service auditd restart
 
-## Neo4j
+## Neo4j - need a specific package repo
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
 sudo apt-get install -y neo4j
 sudo apt-get install -y cypher-shell
 
