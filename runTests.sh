bbf02a16b1d2b21b0f2aa11a241b5031b680043d
Arthur Chan <s1536869@inf.ed.ac.uk>
2018-04-18 10:23:23 -0700

    Fix auto running script for full result

--- runTests.sh
+++ runTests.sh
@@ -1,13 +1,25 @@
-#!/bin/sh
-sudo ./fullAutomation.py $1 . ./benchmarkProgram/control ./benchmarkProgram/grpRename/cmdRename 2 ./cmdRename.clingo
-sudo ./fullAutomation.py $1 . ./benchmarkProgram/control ./benchmarkProgram/grpOpenClose/cmdOpen 2 ./cmdOpen.clingo
-sudo ./fullAutomation.py $1 . ./benchmarkProgram/control ./benchmarkProgram/grpDup/cmdDup 2 ./cmdDup.clingo
-sudo ./fullAutomation.py $1 . ./benchmarkProgram/control ./benchmarkProgram/grpSetuid/cmdSetuid 2 ./cmdSetuid.clingo
-sudo ./fullAutomation.py $1 . ./benchmarkProgram/control ./benchmarkProgram/grpSetgid/cmdSetgid 2 ./cmdSetgid.clingo
+#!/bin/bash
 
-genClingoGraph/clingo2Dot.py cmdRename.clingo cmdRename.dot
-genClingoGraph/clingo2Dot.py cmdOpen.clingo cmdOpen.dot
-genClingoGraph/clingo2Dot.py cmdDup.clingo cmdDup.dot
-genClingoGraph/clingo2Dot.py cmdSetuid.clingo cmdSetuid.dot
-genClingoGraph/clingo2Dot.py cmdSetgid.clingo cmdSetgid.dot
+if [ "$#" -ne 2 ]
+then
+	echo "Usage: "$0" <Tools> <Tools_Path>"
+	exit 1
+fi
 
+mkdir result
+
+for i in `ls benchmarkProgram/baseSyscall`
+do
+	for j in `ls benchmarkProgram/baseSyscall/$i`
+	do
+		syscall=${j:3}
+		echo "Generating provenance benchmark for $syscall in group $i using $1 settings..."
+		sudo ./fullAutomation.py $1 $2 benchmarkProgram/baseSyscall/$i/$j 
+		genClingoGraph/clingo2Dot.py result.clingo result.dot
+		dot -Tsvg -o "${syscall,,}.svg" result.dot
+		rm -f result.clingo
+		rm -f  result.dot
+		mv "${syscall,,}.svg" result/
+		echo "Process complete for $syscall. Result stored in result/${syscall,,}.svg"
+	done
+done
