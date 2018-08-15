aa204763fcb1fbc3e47a31cd08c57bda01fb57c9
root <root@fedora27-vbox.localdomain>
2018-08-15 16:08:01 +0100

    Fix bug

--- startTool/startCamflow.py
+++ startTool/startCamflow.py
@@ -87,7 +87,7 @@ def startCamflow(stagePath, workingPath, suffix, isModel):
 
 	#Capture provenance
 	subprocess.call('service camflowd start'.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-#	subprocess.call('camflow --opaque-file /usr/bin/rm true'.split())
+	subprocess.call('camflow --opaque-file /usr/bin/bash false'.split())
 	subprocess.call(('camflow --track-file %s/test propagate' % stagePath).split())
 	subprocess.call('camflow --duplicate true'.split())
 	subprocess.call('camflow -e true'.split())
