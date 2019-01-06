106b35ba4f6dfbf78e93335ce54b8f0b22ac1fc0
Arthur Chan <s1536869@mogan.inf.ed.ac.uk>
2019-01-06 20:13:59 +0000

    Fix opus vagrant file

--- vagrant/opus/opus-initialize.sh
+++ vagrant/opus/opus-initialize.sh
@@ -13,6 +13,14 @@ sudo apt-get install -y neo4j
 sudo apt-get install -y cypher-shell
 sudo apt-get install -y git
 
+## Install Java 8
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y upgrade
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections 
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -y install oracle-java8-installer
+
 ## ProvMark stuff
 sudo apt-get install -y python3-pip 
 sudo apt-get install -y realpath
