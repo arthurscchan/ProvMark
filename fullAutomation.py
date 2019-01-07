349fe341d59cbd595d1bd2c856cf8e6ec153e154
Arthur Chan <s1536869@mogan.inf.ed.ac.uk>
2019-01-07 18:56:49 +0000

    Make filter graph as config value

--- fullAutomation.py
+++ fullAutomation.py
@@ -15,9 +15,6 @@ def helpMenu(name):
 	print ('Benchmark Directory: Base directory of the benchmark program')
 	print ('Trial:	Number of trial executed for each graph for generalization (Default: 2)')
 
-#change to enable fitlering non-isomorphic graphs
-filterGraphs = False
-
 #Prepare stage and working directory
 def prepareDir(directory):
 	if os.path.exists(directory):
@@ -54,6 +51,7 @@ config.read('%s/config/config.ini' % baseDir)
 
 stage1Tool = config[tool]['stage1tool']
 stage2Handler = config[tool]['stage2handler']
+filterGraphs = config.getboolean(tool, 'filtergraphs')
 
 #Stage 1 - Start the tools and generate graph (neo4j / dot / provjson)
 start = time.time()
