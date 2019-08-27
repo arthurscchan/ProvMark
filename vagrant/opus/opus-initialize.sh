4a7abb31619e1481e2666c23983e79dc9ba2c364
Arthur Chan <s1536869@svente.inf.ed.ac.uk>
2019-08-27 18:38:15 +0100

    Fix init script

--- vagrant/opus/opus-initialize.sh
+++ vagrant/opus/opus-initialize.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-
 #Retrieve OPUS Package and Extract the TARBALL
 echo "Warning: Please get a licensed OPUS copy and extract to any location"
 
-## Neo4j - need a specific package repo
-wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+## Basic dependency
 sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install neo4j=2.2.4
-#sudo apt-get -y install cypher-shell
 sudo apt-get -y install git
 
 ## Install OpenJDK 8
@@ -19,13 +12,18 @@ sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 
-## ProvMark stuff
-sudo apt-get -y install python3-pip 
+## Neo4j - need a specific package repo
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+sudo apt-get -y install neo4j=2.2.4
 sudo apt-get -y install realpath
 sudo apt-get -y install trace-cmd
 sudo apt-get -y install Graphviz
-sudo pip3 install setuptools --upgrade
-sudo pip3 install json_merger
+#sudo apt-get -y install cypher-shell
+
+## ProvMark stuff
+cd ~
 git clone https://github.com/arthurscchan/ProvMark.git 
 cd ProvMark
 git checkout Middleware2019
